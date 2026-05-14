cea5290fc4b46f3339fa40b41a59f1f950d05212
Gakki <gakki@MacBook-Air-6.local>
2026-05-14 18:17:54 +0800

    Add hotel inventory quick entry page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import HotelInventoryPage from "./hotel-inventory/HotelInventoryPage";
 import ThemeGroupDashboard from "./theme-groups/ThemeGroupDashboard";
 
 export default function App() {
+  if (window.location.pathname === "/hotel-inventory") {
+    return <HotelInventoryPage />;
+  }
+
   return <ThemeGroupDashboard />;
 }
